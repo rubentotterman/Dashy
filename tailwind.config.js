fc8f70f21bd74bd3adc72d8643c25449d3d860ed
rubentotterman <ruben_totterman@hotmail.com>
2025-01-23 17:58:07 +0100

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+<<<<<<< HEAD
     extend: {},
+=======
+    extend: {
+      backgroundColor: {
+        'custom-black': '#010101'
+      }
+    },
+>>>>>>> b442312 (Initial commit)
   },
   plugins: [],
 }

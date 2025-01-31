56ec429e6ada4dd3a4ae874f0fcbf3c669f86d46
rubentotterman <ruben_totterman@hotmail.com>
2025-01-31 12:34:36 +0100

    debug

--- src/js/login.js
+++ src/js/login.js
@@ -10,25 +10,32 @@ document.addEventListener('DOMContentLoaded', () => {
         const userInput = document.getElementById('userInput').value;
         const password = document.getElementById('password').value;
 
-        console.log('Attempting to sign in with:', {
-            userInput: userInput,
-            passwordLength: password.Length
-        });
+        console.log('Attempting login with:', { email: userInput, password: password });
 
         try {
+            
+            console.log('Attempting to sign in with:',  { email: userInput});
+
             const { data, error } = await supabase.auth.signInWithPassword({
                 email: userInput,
                 password: password
             });
 
             if (error) {
+                console.log('Auth Error Details:', error);
                 throw error;
             }
 
+            console.log('Sign in Successful:', data);
             window.location.href = 'dashboard.html';
             
         } catch (error) {
             console.error('Error signing in:', error.message);
+
+            const errorDisplay = document.getElementById('error-message');
+            if (errorDisplay) {
+                errorDisplay.textContent = error.message;
+            }
         }
     });
 });
